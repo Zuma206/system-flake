e33186535ce7d9ebca705f19de9bb84d1da9b827
Zuma <zuma@zuma.eu.org>
2025-01-19 05:53:39 +0000

    Added keymap to check if treesitter is active

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "go",
-        "json",
-        "nix",
-        "typescript",
-        "java",
-      },
-      sync_install = false,
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"go",
+				"json",
+				"nix",
+				"typescript",
+				"java",
+			},
+			sync_install = false,
+		})
+
+		vim.keymap.set("n", "<leader>t", function()
+			if type(vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()]) == "table" then
+				print("Treesitter is active for the current buffer")
+			else
+				print("Treesitter is NOT active for the current buffer")
+			end
+		end)
+	end,
 }
